79a312475d12d65e6991dd13b22f5967d77b2376
Mopidevi Sumanth Teja <msteja1002@gmail.com>
2026-07-18 11:34:10 +0530

    Convert Python folder to regular files

---
+++ Python/D5.py
@@ -0,0 +1,22 @@
+#set1=(1,2,3,4,5,1,2,3,4,5)
+#print(set1)
+
+#print(hash(1))
+#unordered:
+#names=['Umamahesh','sumanth','Venketash']
+#print(names)
+
+#print(hash('umamahesh'))
+#note: hash() is the function we use to know the hash value of an object.
+
+
+#details=['umamahesh','28','Bangalore','sales man']
+#details={'name':'umamahesh', 'age':28, 'city':'Bangalore', 'occupation':'sales man'}
+#print(details)
+
+# we can create an frozenset by passing any sequence to the feozenset() func.
+#list1=[1,2,3,4,5,6]
+#fs1=frozenset(list1)
+#print(fs1)
+
+set1={101,}
